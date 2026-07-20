3afae671944290a58f7bf6a7dbb531fd4b3d4a5e
Griffin213 <griffinkipruto808@gmail.com>
2026-07-20 20:34:14 +0300

    Add campus knowledge data structure

---
+++ knowledge.js
@@ -0,0 +1,43 @@
+const campusKnowledge = {
+
+    "ltb1": {
+        name: "Lecture Theatre Block 1",
+        response: "Lecture Theatre Block 1 is one of the main lecture buildings. Starting navigation.",
+        destination: "LTB1"
+    },
+
+    "ltb2": {
+        name: "Lecture Theatre Block 2",
+        response: "Lecture Theatre Block 2 is one of the main lecture buildings. Starting navigation.",
+        destination: "LTB2"
+    },
+
+    "ltb3": {
+        name: "Lecture Theatre Block 3",
+        response: "Lecture Theatre Block 3 is one of the main lecture buildings. Starting navigation.",
+        destination: "LTB3"
+    },
+
+    "ltb4": {
+        name: "Lecture Theatre Block 4",
+        response: "Lecture Theatre Block 4 is one of the main lecture buildings. Starting navigation.",
+        destination: "LTB4"
+    },
+
+    "library": {
+        response: "The library information will be added soon."
+    },
+
+    "administration": {
+        response: "The Administration Block is not yet in my database."
+    },
+
+    "cafeteria": {
+        response: "The cafeteria information will be added soon."
+    },
+
+    "hostel": {
+        response: "Hostel information will be available soon."
+    }
+
+};
