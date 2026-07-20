755328e7918d5cf77de8dca5b3c6ce5ce06e8cd4
Griffin213 <griffinkipruto808@gmail.com>
2026-07-20 13:06:43 +0300

    Add initial HTML structure for campus navigator

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.querySelector("button").addEventListener("click", function(){
     alert("Welcome to the University of Kabianga Smart Campus Navigator!");
 });
+document.getElementById("searchBtn").addEventListener("click", function () {
+
+    let search = document.getElementById("searchBox").value.toLowerCase().trim();
+
+    if (search === "ltb1") {
+        window.location.href = "ltb1.html";
+    }
+
+    else if (search === "ltb2") {
+        window.location.href = "ltb2.html";
+    }
+
+    else if (search === "ltb3") {
+        window.location.href = "ltb3.html";
+    }
+
+    else if (search === "ltb4") {
+        window.location.href = "ltb4.html";
+    }
+
+    else {
+        alert("Venue not found. Please try again.");
+    }
+
+});
