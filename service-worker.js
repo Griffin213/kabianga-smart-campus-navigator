58e7b17efbf56b077b1ee7b1c3ab638e2858fade
Griffin213 <griffinkipruto808@gmail.com>
2026-07-24 19:39:19 +0300

    Add initial HTML structure for campus navigator

--- service-worker.js
+++ service-worker.js
@@ -43,11 +43,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
+        cacheNames.map(cache => caches.delete(cache))
       );
     }).then(() => self.clients.claim())
   );
