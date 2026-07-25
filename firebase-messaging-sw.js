b39edb7b21e0424487b9d3c12de4b48af0828eba
Griffin213 <griffinkipruto808@gmail.com>
2026-07-25 17:37:12 +0300

    Initialize Firebase messaging service worker
    Set up Firebase messaging service worker with notification handling.

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
+  authDomain: "uok-smart-campus-navigator.firebaseapp.com",
+  projectId: "uok-smart-campus-navigator",
+  storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
+  messagingSenderId: "465257479615",
+  appId: "1:465257479615:web:31620a772eff1b603df50a",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/logo.png"
+  });
+});
