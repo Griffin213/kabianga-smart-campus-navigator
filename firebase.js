70a17b3169fbbee1e35ce6ccc5061ce1eb0cd9db
Griffin213 <griffinkipruto808@gmail.com>
2026-07-26 11:24:28 +0300

    Add initial HTML structure for campus navigator

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
@@ -7,25 +8,18 @@ const firebaseConfig = {
   projectId: "uok-smart-campus-navigator",
   storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
   messagingSenderId: "465257479615",
-  appId: "1:465257479615:web:31620a772eff1b603df50a"
+  appId: "1:465257479615:web:31620a772eff1b603df50a",
+  measurementId: "G-971D6JFQNM"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Analytics
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
 const db = getFirestore(app);
 
-onSnapshot(collection(db, "notifications"), (snapshot) => {
-  snapshot.docChanges().forEach((change) => {
-    if (change.type === "added" || change.type === "modified") {
-      const data = change.doc.data();
-
-      if (Notification.permission === "granted") {
-        new Notification(data.Title, {
-          body: data.Message,
-          icon: "logo.jpg"
-        });
-      }
-    }
-  });
-});
-
-export { app, db };
+// Export Firestore
+export { db };
