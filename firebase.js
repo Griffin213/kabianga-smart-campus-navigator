29ff3da53c15bdcc4e61f16577f49cd25c9ada8e
Griffin213 <griffinkipruto808@gmail.com>
2026-07-26 18:37:18 +0300

    Add initial HTML structure for campus navigator

--- firebase.js
+++ firebase.js
@@ -12,5 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-console.log("Firebase connected");
+consoalert("Firebase initialized successfully");
 export { db };
