c9c57d4a8f1c2d766ff89590cbefe92cf9b30787
Griffin213 <griffinkipruto808@gmail.com>
2026-07-28 10:25:10 +0300

    Add initial HTML structure for campus navigator

--- notification.js
+++ notification.js
@@ -1,13 +1,13 @@
-alert("notifications.js loaded");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import {
   getMessaging,
-  getToken,
-  onMessage
+  getToken
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging.js";
 
+alert("notifications.js loaded");
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
   authDomain: "uok-smart-campus-navigator.firebaseapp.com",
   projectId: "uok-smart-campus-navigator",
   storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
@@ -17,22 +17,24 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/kabianga-smart-campus-navigator/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Messaging Service Worker registered");
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
+
+window.requestNotificationPermission = async function () {
+
+    const permission = await Notification.requestPermission();
+
+    if (permission !== "granted") {
+        alert("Notifications denied.");
+        return;
+    }
+
+    const registration = await navigator.serviceWorker.register(
+        "/kabianga-smart-campus-navigator/firebase-messaging-sw.js"
+    );
+
+    const token = await getToken(messaging, {
+        vapidKey: "BAvYs2OcE8SqgpOOICDhV5TxIZyTBRerx8G0-lHOJ-00R2P22Sf3SIMnWxY-ct5pD54yq3gNelfbu2V9qlMpE9Q",
+        serviceWorkerRegistration: registration
     });
-}
-Notification.requestPermission().then((permission) => {
-  if (permission === "granted") {
-    getToken(messaging, {
-      vapidKey: BAvYs2OcE8SqgpOOICDhV5TxIZyTBRerx8G0-lHOJ-00R2P22Sf3SIMnWxY-ct5pD54yq3gNelfbu2V9qlMpE9Q
-    }).then((token) => {
-      console.log("FCM Token:", token);
-      alert(token);
-    });
-  }
-});
+
+    alert("FCM Token:\n\n" + token);
+};
