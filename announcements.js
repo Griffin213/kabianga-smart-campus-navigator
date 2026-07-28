19037a0ef9146859a0bcb28421755b49165e2965
Griffin213 <griffinkipruto808@gmail.com>
2026-07-28 09:47:20 +0300

    Add initial HTML structure for campus navigator

--- announcements.js
+++ announcements.js
@@ -5,3 +5,32 @@ alert("1. announcements.js loaded");
 alert("2. db = " + db);
 
 console.log(db);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  getMessaging,
+  getToken,
+  onMessage
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "uok-smart-campus-navigator.firebaseapp.com",
+  projectId: "uok-smart-campus-navigator",
+  storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
+  messagingSenderId: "465257479615",
+  appId: "1:465257479615:web:31620a772eff1b603df50a"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+Notification.requestPermission().then((permission) => {
+  if (permission === "granted") {
+    getToken(messaging, {
+      vapidKey: "PASTE_YOUR_VAPID_KEY_HERE"
+    }).then((token) => {
+      console.log("FCM Token:", token);
+      alert(token);
+    });
+  }
+});
