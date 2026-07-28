16289411e7ec006f5961257000f3018ce5106557
Griffin213 <griffinkipruto808@gmail.com>
2026-07-28 11:21:55 +0300

    Add initial HTML structure for campus navigator

--- notification.js
+++ notification.js
@@ -4,8 +4,6 @@ import {
   getToken
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging.js";
 
-alert("notifications.js loaded");
-
 const firebaseConfig = {
   apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
   authDomain: "uok-smart-campus-navigator.firebaseapp.com",
@@ -19,22 +17,33 @@ const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 window.requestNotificationPermission = async function () {
+    try {
 
-    const permission = await Notification.requestPermission();
+        const permission = await Notification.requestPermission();
 
-    if (permission !== "granted") {
-        alert("Notifications denied.");
-        return;
+        if (permission !== "granted") {
+            alert("Notifications denied.");
+            return;
+        }
+
+        const registration = await navigator.serviceWorker.register(
+            "/kabianga-smart-campus-navigator/firebase-messaging-sw.js"
+        );
+
+        const token = await getToken(messaging, {
+            vapidKey: "BAvYs2OcE8SqgpOOICDhV5TxIZyTBRerx8G0-lHOJ-00R2P22Sf3SIMnWxY-ct5pD54yq3gNelfbu2V9qlMpE9Q",
+            serviceWorkerRegistration: registration
+        });
+
+        if (token) {
+            alert("FCM Token:\n\n" + token);
+            console.log(token);
+        } else {
+            alert("No FCM token received.");
+        }
+
+    } catch (error) {
+        alert("Firebase Error:\n" + error.message);
+        console.error(error);
     }
-
-    const registration = await navigator.serviceWorker.register(
-        "/kabianga-smart-campus-navigator/firebase-messaging-sw.js"
-    );
-
-    const token = await getToken(messaging, {
-        vapidKey: "BAvYs2OcE8SqgpOOICDhV5TxIZyTBRerx8G0-lHOJ-00R2P22Sf3SIMnWxY-ct5pD54yq3gNelfbu2V9qlMpE9Q",
-        serviceWorkerRegistration: registration
-    });
-
-    alert("FCM Token:\n\n" + token);
 };
