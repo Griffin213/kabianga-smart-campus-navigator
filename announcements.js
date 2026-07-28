b8b1b0255cfe77dcb11bd63a4c0743ae52a09157
Griffin213 <griffinkipruto808@gmail.com>
2026-07-28 11:44:57 +0300

    Add initial HTML structure for campus navigator

--- announcements.js
+++ announcements.js
@@ -1,5 +1,29 @@
 import { db } from "./firebase.js";
+import {
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-alert("1. announcements.js loaded");
+const announcementRef = doc(db, "announcements", "announcement1");
 
-alert("2. db = " + db)
+onSnapshot(announcementRef, (docSnap) => {
+
+    if (!docSnap.exists()) return;
+
+    const data = docSnap.data();
+
+    if (Notification.permission === "granted") {
+
+        navigator.serviceWorker.ready.then((registration) => {
+
+            registration.showNotification(data.Title, {
+                body: data.Message,
+                icon: "logo.jpg",
+                badge: "logo.jpg"
+            });
+
+        });
+
+    }
+
+});
