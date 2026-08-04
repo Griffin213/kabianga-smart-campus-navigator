62764a69505b854cd4c0363e7dc0989c735b0ad2
Griffin213 <griffinkipruto808@gmail.com>
2026-08-04 20:31:13 +0300

    Add initial HTML structure for campus navigator

--- announcements.js
+++ announcements.js
@@ -1,10 +1,7 @@
 import { db } from "./firebase.js";
-import {
-  doc,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-const announcementRef = doc(db, "announcements", "announcement1");
+const announcementRef = doc(db, "announcements", "latest");
 
 onSnapshot(announcementRef, (docSnap) => {
 
@@ -12,12 +9,17 @@ onSnapshot(announcementRef, (docSnap) => {
 
     const data = docSnap.data();
 
+    document.getElementById("announcementTitle").textContent = data.title;
+    document.getElementById("announcementMessage").textContent = data.message;
+    document.getElementById("announcementDate").textContent =
+        new Date(data.time).toLocaleString();
+
     if (Notification.permission === "granted") {
 
         navigator.serviceWorker.ready.then((registration) => {
 
-            registration.showNotification(data.Title, {
-                body: data.Message,
+            registration.showNotification(data.title, {
+                body: data.message,
                 icon: "logo.jpg",
                 badge: "logo.jpg"
             });
