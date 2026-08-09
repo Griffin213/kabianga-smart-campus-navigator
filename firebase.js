3fb3bfd05af692d8dbf5b906efab745a7aa9509b
Griffin213 <griffinkipruto808@gmail.com>
2026-08-09 20:54:13 +0300

    Add initial HTML structure for campus navigator

--- firebase.js
+++ firebase.js
@@ -1,16 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+import {
+    initializeFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
-  authDomain: "uok-smart-campus-navigator.firebaseapp.com",
-  projectId: "uok-smart-campus-navigator",
-  storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
-  messagingSenderId: "465257479615",
-  appId: "1:465257479615:web:31620a772eff1b603df50a"
+
+    apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
+
+    authDomain: "uok-smart-campus-navigator.firebaseapp.com",
+
+    projectId: "uok-smart-campus-navigator",
+
+    storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
+
+    messagingSenderId: "465257479615",
+
+    appId: "1:465257479615:web:31620a772eff1b603df50a"
+
 };
 
+
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+
+// Firestore connection
+const db = initializeFirestore(app, {
+
+    experimentalForceLongPolling: true,
+
+    useFetchStreams: false
+
+});
+
+
+console.log("🔥 Firebase Firestore initialized successfully");
+
 
 export { db };
