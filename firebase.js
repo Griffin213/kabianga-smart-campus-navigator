ddfb2d003db0f3fa2eabc58b6ecbd673de3058ba
Griffin213 <griffinkipruto808@gmail.com>
2026-08-09 21:00:15 +0300

    Add initial HTML structure for campus navigator

--- firebase.js
+++ firebase.js
@@ -6,32 +6,21 @@ import {
 
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyDJpH9mVVuB6zImuPC5SPlz-ETNpuCsNrY",
-
     authDomain: "uok-smart-campus-navigator.firebaseapp.com",
-
     projectId: "uok-smart-campus-navigator",
-
     storageBucket: "uok-smart-campus-navigator.firebasestorage.app",
-
     messagingSenderId: "465257479615",
-
     appId: "1:465257479615:web:31620a772eff1b603df50a"
-
 };
 
 
 const app = initializeApp(firebaseConfig);
 
 
-// Firestore connection
 const db = initializeFirestore(app, {
-
     experimentalForceLongPolling: true,
-
     useFetchStreams: false
-
 });
 
 
